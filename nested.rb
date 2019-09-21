d13f12bf3b8af1f49a658af5d04a57811dde465c
Sarah Glotzbach <sarah.glotzbach@gmail.com>
2019-09-21 16:56:55 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -83,7 +83,6 @@ def adding_matz
   
     programmer_hash[:yukihiro_matsumoto][:known_for] ="Ruby"
     programmer_hash[:yukihiro_matsumoto][:languages]=["LISP", "C"]
-  
     
 end
 
@@ -105,7 +104,6 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      programmer_hash[:alan_kay][:known_for] = "GUI"
-     
      
 end
 
